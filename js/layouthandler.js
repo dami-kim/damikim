90acd24643b828eb091644e22bcd503c2044fed1
dami-kim <62894564+dami-kim@users.noreply.github.com>
2022-10-12 00:10:17 +0900

    Add files via upload

---
+++ js/layouthandler.js
@@ -0,0 +1,8 @@
+if (screen.width <= 700){
+    document.getElementById("pagestyle").setAttribute("href", "/desktop.css");
+} else {   
+    document.getElementById("pagestyle").setAttribute("href", "/desktop.css");
+} 
+  
+
+  
